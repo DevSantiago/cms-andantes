09588f3c44cc1f4cc203170381638fc5445f81f8
DevSantiago <devsantiago13@gmail.com>
2024-10-01 01:19:50 -0500

    added boletins and events cms

---
+++ src/api/boletin/routes/boletin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * boletin router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::boletin.boletin');
---
+++ src/api/boletin/services/boletin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * boletin service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::boletin.boletin');
